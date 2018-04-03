1a1a2327c120bbdda56810908245fb370cac309c
imysheniuk <i.mysheniuk@gmail.com>
2018-04-03 16:34:20 +0300

    PROD-59: initialized rspec and removed the test folder

--- test/test_helper.rb
+++
@@ -1,9 +0,0 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../config/environment', __dir__)
-require 'rails/test_help'
-
-class ActiveSupport
-  module TestCase
-    fixtures :all
-  end
-end
