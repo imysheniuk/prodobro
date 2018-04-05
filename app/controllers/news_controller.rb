b7f45e1974acaa6c67ac75ffc002a5f36610ce03
VitaliiChverenko <vitchv96@gmail.com>
2018-04-05 12:30:21 +0300

    Converted into haml

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,30 +1,31 @@
 class NewsController < ApplicationController
   def index
-    @news = News.all
+   # @news = News.all
   end
   
   def show
-    @news = News.find(params[:id])
+   # @new = News.find(params[:id])
   end
 
-  def new
-    @news = News.new
-  end
+  # def new
+  #   @news = News.new
+  # end
 
-  def edit
-    @news = News.find(params[:id])
-  end
+  # def edit
+  #   @news = News.find(params[:id])
+  # end
 
-  def create
-    @news = News.new(news_params)
-    if @news.save
-      redirect_to @news
-    else
-      render 'new'
-  end
+  # def create
+  #   @news = News.new(news_params)
+  #   if @news.save
+  #     redirect_to @news
+  #   else
+  #     render 'new'
+  #   end
+  # end
 
-  private
-    def news_params
-      params.require(:news).permit(:name, :description, :created_at, :updated_at)
-    end
+  # private
+  #   def news_params
+  #     params.require(:news).permit(:name, :description, :created_at, :updated_at)
+  #   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+<<<<<<< e8bd6d40745ef9cb7c98c0ac51cae38794ed939e
   devise_for :users
+=======
+  get 'news/index'
+  get 'news/show'
+>>>>>>> Converted into haml
 end
