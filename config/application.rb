14a44eb2ae955df22aa0536073d218ac084bf811
mm1krob <mmikrob@gmail.com>
2018-04-10 17:11:26 +0300

    develop_PROD-81: config i18n

--- config/application.rb
+++ config/application.rb
@@ -4,8 +4,10 @@ require 'rails/all'
 
 Bundler.require(*Rails.groups)
 
-module Tmp
+module ProDobro
   class Application < Rails::Application
     config.load_defaults 5.1
+    config.i18n.available_locales = %i[en uk]
+    config.i18n.default_locale = :en
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  scope '(:locale)', locale: /en|uk/ do
+    devise_for :users
+  end
 end
