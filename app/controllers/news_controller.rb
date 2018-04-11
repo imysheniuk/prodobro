a12b4a2516e6c117b0feba03b57578a693970193
VitaliiChverenko <vitchv96@gmail.com>
2018-04-11 12:38:01 +0300

    Pulled from develop

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,32 +1,47 @@
 class NewsController < ApplicationController
   def index
-   # @news = News.all
     @news = News.order(:id).page(params[:page])
   end
   
   def show
-   # @new = News.find(params[:id])
+    @new = News.find(params[:id])
   end
 
-  # def new
-  #   @news = News.new
-  # end
+  def new
+    @new = News.new
+  end
 
-  # def edit
-  #   @news = News.find(params[:id])
-  # end
+  def edit
+    @new = News.find(params[:id])
+  end
 
-  # def create
-  #   @news = News.new(news_params)
-  #   if @news.save
-  #     redirect_to @news
-  #   else
-  #     render 'new'
-  #   end
-  # end
+  def update
+    @new = News.find(params[:id])
+    if @new.update(news_params)
+      redirect_to @new
+    else
+      render 'edit'
+    end
+  end
 
-  # private
-  #   def news_params
-  #     params.require(:news).permit(:name, :description, :created_at, :updated_at)
-  #   end
+  def create
+    @news = News.new(news_params)
+    if @news.save
+      redirect_to @news
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+     @new = News.find(params[:id])
+     @new.destroy
+     redirect_to @new
+   end
+
+  private
+
+  def news_params
+    params.require(:news).permit(:title, :description, :created_at, :updated_at)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< e8bd6d40745ef9cb7c98c0ac51cae38794ed939e
   devise_for :users
-=======
-  get 'news/index'
-  get 'news/show'
->>>>>>> Converted into haml
+  resources :news
 end
