92725f142ae796e0cd3ad07c868f39bfe008e1b6
imysheniuk <i.mysheniuk@gmail.com>
2018-04-11 11:57:49 +0300

    develop_PROD-82: problems fixed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20180410104202) do
-
   create_table "campaigns", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
     t.text "description"
