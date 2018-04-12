f01b52e296ceb713ef942adf36734e2eab8c776b
imysheniuk <i.mysheniuk@gmail.com>
2018-04-12 22:17:46 +0300

    Update factories.rb

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,15 +1,6 @@
 FactoryBot.define do
   factory :campaign do
-    # title Faker::Internet.user_name(16..16)
-    # description Faker::Name.title
-    # status Faker::Name.first_name
-    # current_amount Faker::Number.number(9)
-    # needed_amount Faker::Number.number(9)
-    # requisite Faker::Base.numerify('################')
-    # category_id 1
-    # finished_at Time.now
     title Faker::Book.title
-    # description Faker::Text.realText($maxNbChars = 2000)
     description 'Qwertyuiop' * 50
     status 'MyString'
     current_amount 1234567
@@ -17,8 +8,6 @@ FactoryBot.define do
     requisite '1234567890123456'
     category_id 1
     finished_at Time.zone.now
-
-    
   end
   factory :user do
     name Faker::Name.first_name
